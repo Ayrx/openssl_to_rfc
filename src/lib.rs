d016ddb9f2c293324e1d74c93c32b075720a609a
Terry Chia <terrycwk1994@gmail.com>
2020-04-30 20:25:28 +0800

    Add ability to convert RFC name to OpenSSL name.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
-//! This crate converts an OpenSSL cipher suite name to the equivalent
-//! RFC name.
+//! This crate provides methods to convert OpenSSL cipher suite names into the
+//! equivalent RFC name and vice versa.
 //!
 //! ```
 //! use openssl_to_rfc::TLSCipherSuite;
-//! let cipher = TLSCipherSuite::from_openssl_name("ECDH-RSA-AES128-GCM-SHA256").unwrap();
+//!
+//! let openssl_name = "ECDH-RSA-AES128-GCM-SHA256";
+//! let cipher = TLSCipherSuite::from_openssl_name(openssl_name).unwrap();
+//!
 //! assert_eq!(cipher, TLSCipherSuite::TLS_ECDH_RSA_WITH_AES_128_GCM_SHA256);
+//! assert_eq!(cipher.into_openssl_name(), openssl_name);
 //! ```
+//!
+//! For SSLv2 cipher suites, use the `SSLV2CipherSuite` enum instead.
 
 mod ssl2;
 pub use ssl2::SSLV2CipherSuite;
@@ -31,6 +37,24 @@ mod tests {
             }
 
             let c = TLSCipherSuite::from_openssl_name(openssl_name).unwrap();
+            assert_eq!(c, cipher);
+        }
+    }
+
+    #[test]
+    fn round_trip_tls_names() {
+        for cipher in TLSCipherSuite::iter() {
+            let openssl_name = cipher.into_openssl_name();
+            let c = TLSCipherSuite::from_openssl_name(openssl_name).unwrap();
+            assert_eq!(c, cipher);
+        }
+    }
+
+    #[test]
+    fn round_trip_ssl2_names() {
+        for cipher in SSLV2CipherSuite::iter() {
+            let openssl_name = cipher.into_openssl_name();
+            let c = SSLV2CipherSuite::from_openssl_name(openssl_name).unwrap();
             assert_eq!(c, cipher);
         }
     }
--- src/ssl2.rs
+++ src/ssl2.rs
@@ -29,4 +29,18 @@ impl SSLV2CipherSuite {
             _ =>  None,
         }
     }
+
+    pub fn into_openssl_name(&self) -> &str {
+        match *self {
+            Self::SSL_CK_RC4_128_WITH_MD5 => "RC4-MD5",
+            Self::SSL_CK_RC4_128_EXPORT40_WITH_MD5 => "EXP-RC4-MD5",
+            Self::SSL_CK_RC2_128_CBC_WITH_MD5 => "RC2-CBC-MD5",
+            Self::SSL_CK_RC2_128_CBC_EXPORT40_WITH_MD5 => "EXP-RC2-CBC-MD5",
+            Self::SSL_CK_IDEA_128_CBC_WITH_MD5 => "IDEA-CBC-MD5",
+            Self::SSL_CK_DES_64_CBC_WITH_MD5 => "DES-CBC-MD5",
+            Self::SSL_CK_DES_192_EDE3_CBC_WITH_MD5 => "DES-CBC3-MD5",
+            Self::SSL_CK_RC4_64_WITH_MD5 => "RC4-64-MD5",
+            Self::TLS_RSA_WITH_NULL_MD5 => "NULL-MD5",
+        }
+    }
 }
