40ee81fe46ef23f49a7b2a779418caaee243dd40
Terry Chia <terrycwk1994@gmail.com>
2020-07-12 21:16:19 +0800

    Run `cargo fmt`.

--- src/ssl2.rs
+++ src/ssl2.rs
@@ -18,15 +18,15 @@ impl SSLV2CipherSuite {
     pub fn from_openssl_name(name: &str) -> Option<SSLV2CipherSuite> {
         match name {
             "RC4-MD5" => Some(Self::SSL_CK_RC4_128_WITH_MD5),
-            "EXP-RC4-MD5"=> Some(Self::SSL_CK_RC4_128_EXPORT40_WITH_MD5),
-            "RC2-CBC-MD5"=> Some(Self::SSL_CK_RC2_128_CBC_WITH_MD5),
+            "EXP-RC4-MD5" => Some(Self::SSL_CK_RC4_128_EXPORT40_WITH_MD5),
+            "RC2-CBC-MD5" => Some(Self::SSL_CK_RC2_128_CBC_WITH_MD5),
             "EXP-RC2-CBC-MD5" => Some(Self::SSL_CK_RC2_128_CBC_EXPORT40_WITH_MD5),
             "IDEA-CBC-MD5" => Some(Self::SSL_CK_IDEA_128_CBC_WITH_MD5),
             "DES-CBC-MD5" => Some(Self::SSL_CK_DES_64_CBC_WITH_MD5),
             "DES-CBC3-MD5" => Some(Self::SSL_CK_DES_192_EDE3_CBC_WITH_MD5),
             "RC4-64-MD5" => Some(Self::SSL_CK_RC4_64_WITH_MD5),
             "NULL-MD5" => Some(Self::TLS_RSA_WITH_NULL_MD5),
-            _ =>  None,
+            _ => None,
         }
     }
 
